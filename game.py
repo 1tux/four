9c01ec9ed058b84488c756b7a4725af4a70604fb
itay <itayona@gmail.com>
2022-02-26 17:50:48 +0200

    moved to python36 to work with pytorch

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3.6
 import numpy as np
 import string
 
---
+++ net.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3.6
+# based on the RL courses:
+# 2015 Intro to RL: https://www.youtube.com/playlist?list=PLqYmG7hTraZDM-OYHWgPebj2MfCFzFObQ
+# 2021 DeepMind x UCL RL Lecture Series  - https://www.youtube.com/watch?v=TCCjZe0y4Qc&list=PLqYmG7hTraZDVH599EItlEWsUOsJbAodm&ab_channel=DeepMind
+
+import torch
